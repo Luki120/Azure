020b391caa8cf1065dc65a4bd50618095f450397
luki120 <luki120@exerra.xyz>
2022-02-17 04:44:22 -0300

    more constants

--- Constants/Constants.h
+++ Constants/Constants.h
@@ -2,6 +2,9 @@
 
 
 static NSString *const kIdentifier = @"AzurePinCodeCell";
+static NSString *const kCheckra1n = @"/var/checkra1n.dmg";
+static NSString *const kTaurine = @"/Taurine";
+static NSString *const kUnc0ver = @"/private/etc/apt/undecimus";
 
 #define kAzureTintColor UIColor.systemTealColor
 #define kUserInterfaceStyle self.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark
