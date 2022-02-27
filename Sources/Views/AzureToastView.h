ddcf9ed651d9a17f7958326f7e14aeed19a6f9c0
luki120 <luki120@exerra.xyz>
2022-02-27 06:56:49 -0300

    stuff
    • made a generic animation function to reduce code duplication between files
    • moved animation func to a UIView category to make it more reusable

--- Sources/Views/AzureToastView.h
+++ Sources/Views/AzureToastView.h
@@ -1,5 +1,12 @@
+#import "Sources/Categories/UIView+Animations.h"
 #import "Sources/Constants/Constants.h"
 
 
-@interface AzureToastView : UIView
+@interface AzureToastView : UIView {
+
+	@public UILabel *toastViewLabel;
+
+}
+- (void)fadeInOutToastView;
+- (void)animateToastViewWithConstraintConstant:(CGFloat)constant andAlpha:(CGFloat)alpha;
 @end
--- Sources/Views/AzureToastView.m
+++ Sources/Views/AzureToastView.m
@@ -3,8 +3,7 @@
 
 @implementation AzureToastView {
 
-	UIView *copyPinToastView;
-	UILabel *copiedPinLabel;
+	UIView *toastView;
 	NSLayoutConstraint *bottomAnchorConstraint;
 
 }
@@ -18,7 +17,7 @@
 	[self setupToastView];
 
 	[NSNotificationCenter.defaultCenter removeObserver:self];
-	[NSNotificationCenter.defaultCenter addObserver:self selector:@selector(fadeToast) name:@"fadeInOutToast" object:nil];
+	[NSNotificationCenter.defaultCenter addObserver:self selector:@selector(fadeInOutCopyPinToastView) name:@"fadeInOutToast" object:nil];
 
 	return self;
 
@@ -29,61 +28,68 @@
 
 	self.translatesAutoresizingMaskIntoConstraints = NO;
 
-	copyPinToastView = [UIView new];
-	copyPinToastView.alpha = 0;
-	copyPinToastView.backgroundColor = kAzureMintTintColor;
-	copyPinToastView.layer.cornerCurve = kCACornerCurveContinuous;
-	copyPinToastView.layer.cornerRadius = 20;
-	copyPinToastView.translatesAutoresizingMaskIntoConstraints = NO;
-	[self addSubview: copyPinToastView];
+	toastView = [UIView new];
+	toastView.alpha = 0;
+	toastView.backgroundColor = kAzureMintTintColor;
+	toastView.layer.cornerCurve = kCACornerCurveContinuous;
+	toastView.layer.cornerRadius = 20;
+	toastView.translatesAutoresizingMaskIntoConstraints = NO;
+	[self addSubview: toastView];
 
-	copiedPinLabel = [UILabel new];
-	copiedPinLabel.font = [UIFont systemFontOfSize: 14];
-	copiedPinLabel.text = @"Copied!";
-	copiedPinLabel.textColor = UIColor.labelColor;
-	copiedPinLabel.textAlignment = NSTextAlignmentCenter;
-	copiedPinLabel.translatesAutoresizingMaskIntoConstraints = NO;
-	[copyPinToastView addSubview: copiedPinLabel];
+	toastViewLabel = [UILabel new];
+	toastViewLabel.font = [UIFont systemFontOfSize: 14];
+	toastViewLabel.textColor = UIColor.labelColor;
+	toastViewLabel.numberOfLines = 0;
+	toastViewLabel.textAlignment = NSTextAlignmentCenter;
+	toastViewLabel.adjustsFontSizeToFitWidth = YES;
+	toastViewLabel.translatesAutoresizingMaskIntoConstraints = NO;
+	[toastView addSubview: toastViewLabel];
 
-	[copyPinToastView.centerXAnchor constraintEqualToAnchor: self.centerXAnchor].active = YES;
-	bottomAnchorConstraint = [copyPinToastView.bottomAnchor constraintEqualToAnchor: self.safeAreaLayoutGuide.bottomAnchor constant : 50];
+	[toastView.centerXAnchor constraintEqualToAnchor: self.centerXAnchor].active = YES;
+	bottomAnchorConstraint = [toastView.bottomAnchor constraintEqualToAnchor: self.safeAreaLayoutGuide.bottomAnchor constant : 50];
 	bottomAnchorConstraint.active = YES;
-	[copyPinToastView.widthAnchor constraintEqualToConstant: 120].active = YES;
-	[copyPinToastView.heightAnchor constraintEqualToConstant: 40].active = YES;
+	[toastView.widthAnchor constraintEqualToConstant: 120].active = YES;
+	[toastView.heightAnchor constraintEqualToConstant: 40].active = YES;
 
-	[copiedPinLabel.centerXAnchor constraintEqualToAnchor: copyPinToastView.centerXAnchor].active = YES;
-	[copiedPinLabel.centerYAnchor constraintEqualToAnchor: copyPinToastView.centerYAnchor].active = YES;
+	[toastViewLabel.centerXAnchor constraintEqualToAnchor: toastView.centerXAnchor].active = YES;
+	[toastViewLabel.centerYAnchor constraintEqualToAnchor: toastView.centerYAnchor].active = YES;
+	[toastViewLabel.leadingAnchor constraintEqualToAnchor: toastView.leadingAnchor constant: 10].active = YES;
+	[toastViewLabel.trailingAnchor constraintEqualToAnchor: toastView.trailingAnchor constant: -10].active = YES;
 
 }
 
 
-- (void)fadeToast {
+- (void)fadeInOutCopyPinToastView {
 
-	[self animateViewWithDelay:0 withAnimations:^ {
+	toastViewLabel.text = @"Copied!";
+	[self fadeInOutToastView];
+
+}
+
+
+- (void)fadeInOutToastView {
+
+	[UIView animateViewWithDelay:0 withAnimations:^ {
 
 		[self animateToastViewWithConstraintConstant: -20 andAlpha: 1];
 
 	} withCompletion:^(BOOL finished) {
 
-		[self animateViewWithDelay:0.2 withAnimations:^ {
+		[UIView animateViewWithDelay:0.2 withAnimations:^ {
 
-			CATransform3D rotation = CATransform3DIdentity;
-			rotation.m34 = 1.0 / - 500; // idfk what this does but ok :lul:
-			rotation = CATransform3DRotate(rotation, 180.0 * M_PI / 180, 0, 1, 0);
-			copyPinToastView.layer.transform = rotation;
-			copiedPinLabel.layer.transform = rotation;
+			[UIView makeRotationTransformForView:toastView andLabel:toastViewLabel];
 			[self layoutIfNeeded];
 
 		} withCompletion:^(BOOL finished) {
 
-			[self animateViewWithDelay:0.5 withAnimations:^ {
+			[UIView animateViewWithDelay:0.5 withAnimations:^ {
 
 				[self animateToastViewWithConstraintConstant: 50 andAlpha: 0];
 
 			} withCompletion:^(BOOL finished) {
 
-				copyPinToastView.layer.transform = CATransform3DIdentity;
-				copiedPinLabel.layer.transform = CATransform3DIdentity;
+				toastView.layer.transform = CATransform3DIdentity;
+				toastViewLabel.layer.transform = CATransform3DIdentity;
 
 			}];
 
@@ -97,17 +103,8 @@
 - (void)animateToastViewWithConstraintConstant:(CGFloat)constant andAlpha:(CGFloat)alpha {
 
 	bottomAnchorConstraint.constant = constant;
-	copyPinToastView.alpha = alpha;
+	toastView.alpha = alpha;
 	[self layoutIfNeeded];
-
-}
-
-
-- (void)animateViewWithDelay:(CGFloat)delay
-	withAnimations:(void (^)(void))animations
-	withCompletion:(void(^)(BOOL finished))completion {
-
-	[UIView animateWithDuration:0.5 delay:delay options:UIViewAnimationOptionCurveEaseIn animations:animations completion:completion];
 
 }
 
