5b4aa2990afd2bb22016e38fe19f7c8ba45f10f5
luki120 <luki120@exerra.xyz>
2022-02-28 07:10:19 -0300

    .

--- Sources/Controllers/Core/AzureTableVC.h
+++ Sources/Controllers/Core/AzureTableVC.h
@@ -3,9 +3,9 @@
 #import "Sources/Categories/UIView+Animations.h"
 #import "Sources/Cells/AzurePinCodeCell.h"
 #import "Sources/Constants/Constants.h"
-#import "Sources/Controllers/Core/PinCodeVC.h"
-#import "Sources/Controllers/Core/QRCodeVC.h"
 #import "Sources/Controllers/Misc/AlgorithmVC.h"
+#import "Sources/Controllers/Misc/PinCodeVC.h"
+#import "Sources/Controllers/Misc/QRCodeVC.h"
 #import "Sources/Views/AzureFloatingButtonView.h"
 #import "Sources/Views/AzureToastView.h"
 
