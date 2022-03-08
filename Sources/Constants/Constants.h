ec5c28555dbfa825ed573426e369f51034687b8f
luki120 <luki120@exerra.xyz>
2022-03-08 20:02:31 -0300

    more constants

--- Sources/Constants/Constants.h
+++ Sources/Constants/Constants.h
@@ -5,6 +5,8 @@ static NSString *const kIdentifier = @"AzurePinCodeCell";
 static NSString *const kCheckra1n = @"/var/checkra1n.dmg";
 static NSString *const kTaurine = @"/taurine";
 static NSString *const kUnc0ver = @"/private/etc/apt/undecimus";
+static NSString *const kAzureDir = @"/var/mobile/Documents/Azure";
+static NSString *const kAzurePath = @"/var/mobile/Documents/Azure/AzureBackup.json";
 
 #define kAzureLilacTintColor [UIColor colorWithRed: 0.70 green: 0.56 blue: 1.0 alpha: 1.0]
 #define kAzureMintTintColor [UIColor colorWithRed: 0.40 green: 0.81 blue: 0.73 alpha: 1.0]
