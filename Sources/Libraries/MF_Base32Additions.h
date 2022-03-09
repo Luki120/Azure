8460e90d378d7c2c42981765cc7442c439842391
luki120 <luki120@exerra.xyz>
2022-03-09 06:10:42 -0300

    fixed Google's code formatting and style

--- Sources/Libraries/MF_Base32Additions.h
+++ Sources/Libraries/MF_Base32Additions.h
@@ -9,21 +9,21 @@
 //  Public Domain
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
-#define NSBase32StringEncoding  0x4D467E32
+#define NSBase32StringEncoding 0x4D467E32
 
 @interface NSString (Base32Addition)
-+(NSString *)stringFromBase32String:(NSString *)base32String;
--(NSString *)base32String;
+- (NSString *)base32String;
++ (NSString *)stringFromBase32String:(NSString *)base32String;
 @end
 
 @interface NSData (Base32Addition)
-+(NSData *)dataWithBase32String:(NSString *)base32String;
--(NSString *)base32String;
+- (NSString *)base32String;
++ (NSData *)dataWithBase32String:(NSString *)base32String;
 @end
 
 @interface MF_Base32Codec : NSObject
-+(NSData *)dataFromBase32String:(NSString *)base32String;
-+(NSString *)base32StringFromData:(NSData *)data;
++ (NSString *)base32StringFromData:(NSData *)data;
++ (NSData *)dataFromBase32String:(NSString *)base32String;
 @end
