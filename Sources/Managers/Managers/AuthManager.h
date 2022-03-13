c12f429826ca7dd96210d598427d7fca517e3b75
luki120 <luki120@exerra.xyz>
2022-03-13 08:20:12 -0300

    properly prevent A11 devices in c1 from getting biometrics

--- Sources/Managers/Managers/AuthManager.h
+++ Sources/Managers/Managers/AuthManager.h
@@ -1,8 +1,10 @@
-@import Foundation;
 @import LocalAuthentication;
+#import <sys/utsname.h>
+#import "Sources/Constants/Constants.h"
 
 
 @interface AuthManager : NSObject
+- (BOOL)shouldUseBiometrics;
 - (void)setupAuthWithReason:(NSString *)reason
 	reply:(void(^)(BOOL success, NSError *error))reply;
 @end
--- Sources/Managers/Managers/AuthManager.m
+++ Sources/Managers/Managers/AuthManager.m
@@ -20,4 +20,23 @@
 
 }
 
+
+- (BOOL)shouldUseBiometrics {
+
+	struct utsname systemInfo;
+	uname(&systemInfo);
+	NSString *deviceModel = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+
+	if([[NSFileManager defaultManager] fileExistsAtPath: kCheckra1n]
+		&& ([deviceModel isEqualToString:@"iPhone10,1"]
+		|| [deviceModel isEqualToString:@"iPhone10,4"]
+		|| [deviceModel isEqualToString:@"iPhone10,2"]
+		|| [deviceModel isEqualToString:@"iPhone10,5"]
+		|| [deviceModel isEqualToString:@"iPhone10,3"]
+		|| [deviceModel isEqualToString:@"iPhone10,6"])) return NO;
+
+	return YES;
+
+}
+
 @end
