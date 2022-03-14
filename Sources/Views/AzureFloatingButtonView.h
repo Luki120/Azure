44266c636d2a4cfca82f50de9142658e08bc5433
luki120 <luki120@exerra.xyz>
2022-03-14 08:40:06 -0300

    idk why this was a property, switched to ivar

--- Sources/Views/AzureFloatingButtonView.h
+++ Sources/Views/AzureFloatingButtonView.h
@@ -3,13 +3,13 @@
 
 @protocol AzureFloatingButtonViewDelegate <NSObject>
 
-@required - (void)didTapFloatingButton;
+@required
+- (void)azureFloatingButtonViewDidTapFloatingButton;
 
 @end
 
 
 @interface AzureFloatingButtonView : UIView
-@property (nonatomic, strong) UIButton *floatingCreateButton;
 @property (nonatomic, weak) id <AzureFloatingButtonViewDelegate> delegate;
 - (void)animateViewWithAlpha:(CGFloat)alpha translateX:(CGFloat)tx translateY:(CGFloat)ty;
 @end
--- Sources/Views/AzureFloatingButtonView.m
+++ Sources/Views/AzureFloatingButtonView.m
@@ -1,7 +1,11 @@
 #import "AzureFloatingButtonView.h"
 
 
-@implementation AzureFloatingButtonView
+@implementation AzureFloatingButtonView {
+
+	UIButton *floatingCreateButton;
+
+}
 
 - (id)init {
 
@@ -18,46 +22,44 @@
 
 	self.translatesAutoresizingMaskIntoConstraints = NO;
 
-	self.floatingCreateButton = [UIButton new];
-	self.floatingCreateButton.tintColor = UIColor.labelColor;
-	self.floatingCreateButton.backgroundColor = kAzureMintTintColor;
-	self.floatingCreateButton.layer.shadowColor = kUserInterfaceStyle ? UIColor.whiteColor.CGColor : UIColor.blackColor.CGColor;
-	self.floatingCreateButton.layer.cornerRadius = 30;
-	self.floatingCreateButton.layer.shadowRadius = 8;
-	self.floatingCreateButton.layer.shadowOffset = CGSizeMake(0, 1);
-	self.floatingCreateButton.layer.shadowOpacity = 0.5;
-	self.floatingCreateButton.translatesAutoresizingMaskIntoConstraints = NO;
-	[self.floatingCreateButton setImage: [UIImage systemImageNamed:@"plus" withConfiguration: [UIImageSymbolConfiguration configurationWithPointSize: 25]] forState: UIControlStateNormal];
-	[self.floatingCreateButton addTarget:self action:@selector(didTapButton) forControlEvents: UIControlEventTouchUpInside];
-	[self addSubview: self.floatingCreateButton];
+	floatingCreateButton = [UIButton new];
+	floatingCreateButton.tintColor = UIColor.labelColor;
+	floatingCreateButton.backgroundColor = kAzureMintTintColor;
+	floatingCreateButton.layer.shadowColor = kUserInterfaceStyle ? UIColor.whiteColor.CGColor : UIColor.blackColor.CGColor;
+	floatingCreateButton.layer.cornerRadius = 30;
+	floatingCreateButton.layer.shadowRadius = 8;
+	floatingCreateButton.layer.shadowOffset = CGSizeMake(0, 1);
+	floatingCreateButton.layer.shadowOpacity = 0.5;
+	floatingCreateButton.translatesAutoresizingMaskIntoConstraints = NO;
+	[floatingCreateButton setImage: [UIImage systemImageNamed:@"plus" withConfiguration:[UIImageSymbolConfiguration configurationWithPointSize:25]] forState: UIControlStateNormal];
+	[floatingCreateButton addTarget:self action:@selector(didTapButton) forControlEvents: UIControlEventTouchUpInside];
+	[self addSubview: floatingCreateButton];
 
-	[self.floatingCreateButton.topAnchor constraintEqualToAnchor: self.topAnchor].active = YES;
-	[self.floatingCreateButton.bottomAnchor constraintEqualToAnchor: self.bottomAnchor].active = YES;
-	[self.floatingCreateButton.leadingAnchor constraintEqualToAnchor: self.leadingAnchor].active = YES;
-	[self.floatingCreateButton.trailingAnchor constraintEqualToAnchor: self.trailingAnchor].active = YES;
+	[floatingCreateButton.topAnchor constraintEqualToAnchor: self.topAnchor].active = YES;
+	[floatingCreateButton.bottomAnchor constraintEqualToAnchor: self.bottomAnchor].active = YES;
+	[floatingCreateButton.leadingAnchor constraintEqualToAnchor: self.leadingAnchor].active = YES;
+	[floatingCreateButton.trailingAnchor constraintEqualToAnchor: self.trailingAnchor].active = YES;
 
 }
+
 
 - (void)traitCollectionDidChange:(UITraitCollection *)previousTraitCollection {
 
 	[super traitCollectionDidChange: previousTraitCollection];
-	self.floatingCreateButton.layer.shadowColor = kUserInterfaceStyle ? UIColor.whiteColor.CGColor : UIColor.blackColor.CGColor;
+	floatingCreateButton.layer.shadowColor = kUserInterfaceStyle ? UIColor.whiteColor.CGColor : UIColor.blackColor.CGColor;
 
 }
 
-- (void)didTapButton {
 
-	[self.delegate didTapFloatingButton];
-
-}
+- (void)didTapButton { [self.delegate azureFloatingButtonViewDidTapFloatingButton]; }
 
 
 - (void)animateViewWithAlpha:(CGFloat)alpha translateX:(CGFloat)tx translateY:(CGFloat)ty {
 
 	[UIView animateWithDuration:0.5 delay:0 usingSpringWithDamping:0.6 initialSpringVelocity:0.1 options:UIViewAnimationOptionTransitionCrossDissolve animations:^{
 
-		self.floatingCreateButton.alpha = alpha;
-		self.floatingCreateButton.transform = CGAffineTransformMakeTranslation(tx, ty);
+		floatingCreateButton.alpha = alpha;
+		floatingCreateButton.transform = CGAffineTransformMakeTranslation(tx, ty);
 
 	} completion:nil];
 
