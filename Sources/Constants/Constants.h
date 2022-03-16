2e2723c8f751b0b85ff61681f3c9fd12c7438309
luki120 <luki120@exerra.xyz>
2022-03-16 03:54:27 -0300

    another constant

--- Sources/Constants/Constants.h
+++ Sources/Constants/Constants.h
@@ -8,6 +8,7 @@ static NSString *const kUnc0ver = @"/private/etc/apt/undecimus";
 static NSString *const kAzureDir = @"/var/mobile/Documents/Azure";
 static NSString *const kAzurePath = @"/var/mobile/Documents/Azure/AzureBackup.json";
 static NSString *const kImagePath = @"/var/mobile/Documents/Azure/Assets/lock.png";
+static NSString *const kAzureReasonSensitiveOperation = @"Azure needs you to authenticate in order to verify your identity for a sensitive operation.";
 
 #define kAzureLilacTintColor [UIColor colorWithRed: 0.70 green: 0.56 blue: 1.0 alpha: 1.0]
 #define kAzureMintTintColor [UIColor colorWithRed: 0.40 green: 0.81 blue: 0.73 alpha: 1.0]
