26084398b4c1dce700e406818080d0593d522022
luki120 <luki120@exerra.xyz>
2024-06-07 21:32:16 -0300

    yes • implemented a clear all button with clean good looking animations for the NewIssuerFormCell's text fields • strip whitespace from text that gets pasted (or typed) in the secret's textField

--- Sources/Views/Cells/NewIssuerFormCell.swift
+++ Sources/Views/Cells/NewIssuerFormCell.swift
@@ -10,9 +10,42 @@ final class NewIssuerFormCell: UITableViewCell {
 		textField.font = .systemFont(ofSize: 16)
 		textField.delegate = self
 		textField.leftView = UIView(frame: .init(x: 0, y: 0, width: 20, height: textField.frame.size.height))
+		textField.rightView = UIView(frame: .init(x: 0, y: 0, width: 20, height: textField.frame.size.height))
 		textField.leftViewMode = .always
-		addSubview(textField)
+		textField.rightViewMode = .always
+		textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+		contentView.addSubview(textField)
 		return textField
+	}()
+
+	private lazy var clearAllButton: UIButton = {
+		let button = UIButton()
+		if #available(iOS 15.0, *) {
+			var configuration: UIButton.Configuration = .plain()
+			configuration.image = UIImage(systemName: "xmark.circle.fill") ?? UIImage()
+			configuration.baseForegroundColor = kUserInterfaceStyle == .dark ? .tertiarySystemBackground : .lightColor
+			button.configuration = configuration
+		}
+		else {
+			let configuration = UIImage.SymbolConfiguration(pointSize: 20)
+
+			button.tintColor = kUserInterfaceStyle == .dark ? .tertiarySystemBackground : .lightColor
+			button.setImage(.init(systemName: "xmark.circle.fill", withConfiguration: configuration) ?? UIImage(), for: .normal)
+		}
+		button.alpha = 0
+		button.addAction(
+			UIAction { [weak self] _ in
+				guard let self else { return }
+				UIView.transition(with: button, duration: 0.35, options: .transitionCrossDissolve) {
+					button.alpha = 0
+					self.cleanTextField.text = ""
+				}
+			},
+			for: .touchUpInside
+		)
+		button.translatesAutoresizingMaskIntoConstraints = false
+		contentView.addSubview(button)
+		return button
 	}()
 
 	var completion: ((String) -> Void)?
@@ -35,7 +68,7 @@ final class NewIssuerFormCell: UITableViewCell {
 
 	override func layoutSubviews() {
 		super.layoutSubviews()
-		pinViewToAllEdges(cleanTextField)
+		layoutUI()
 
 		contentView.layer.cornerCurve = .continuous
 		contentView.layer.cornerRadius = 14
@@ -46,12 +79,36 @@ final class NewIssuerFormCell: UITableViewCell {
 	override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
 		super.traitCollectionDidChange(previousTraitCollection)
 		layer.shadowColor = kUserInterfaceStyle == .dark ? .darkShadowColor : .lightShadowColor
+
+		if #available(iOS 15.0, *) {
+			clearAllButton.configuration?.baseForegroundColor = kUserInterfaceStyle == .dark ? .tertiarySystemBackground : .lightColor
+		}
+		else {
+			clearAllButton.tintColor = kUserInterfaceStyle == .dark ? .tertiarySystemBackground : .lightColor
+		}
 	}
 
 	// ! Private
 
+	private func layoutUI() {
+		contentView.pinViewToAllEdges(cleanTextField)		
+
+		clearAllButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor).isActive = true
+		clearAllButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15).isActive = true
+
+		setupSizeConstraints(forView: clearAllButton, width: 30, height: 30)
+	}
+
 	@objc private func resignResponder() {
 		cleanTextField.resignFirstResponder()
+	}
+
+	@objc private func textFieldDidChange(_ textField: UITextField) {
+		guard let text = textField.text else { return }
+
+		UIView.transition(with: clearAllButton, duration: 0.35, options: .transitionCrossDissolve) {
+			self.clearAllButton.alpha = text.count > 0 ? 1 : 0
+		}
 	}
 
 }
@@ -75,18 +132,57 @@ extension NewIssuerFormCell {
 
 extension NewIssuerFormCell: UITextFieldDelegate {
 
+	func textFieldDidBeginEditing(_ textField: UITextField) {
+		if textField.text?.count ?? 0 > 1 {
+			UIView.transition(with: clearAllButton, duration: 0.35, options: .transitionCrossDissolve) {
+				self.clearAllButton.alpha = 1
+			}
+		}
+	}
+
 	func textFieldDidEndEditing(_ textField: UITextField) {
+		UIView.transition(with: clearAllButton, duration: 0.35, options: .transitionCrossDissolve) {
+			self.clearAllButton.alpha = 0
+		}
 		completion?(textField.text ?? "")
 	}
 
 	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-		guard let nextTextField = textField.superview?.superview?.viewWithTag(textField.tag + 1) as? UITextField else {
+		guard let nextTextField = textField.superview?.superview?.superview?.viewWithTag(textField.tag + 1) as? UITextField else {
 			textField.resignFirstResponder()
 			return false
 		}
 
 		nextTextField.becomeFirstResponder()
 		return true
+	}
+
+	func textField(
+		_ textField: UITextField,
+		shouldChangeCharactersIn range: NSRange,
+		replacementString string: String
+	) -> Bool {
+
+		guard textField.tag == 2, let text = textField.text, let range = Range(range, in: text) else { return true }
+
+		if string.count > 1 {
+			UIView.transition(with: clearAllButton, duration: 0.35, options: .transitionCrossDissolve) {
+				self.clearAllButton.alpha = 1
+			}
+		}
+
+		let strippedText = string.replacingOccurrences(of: " ", with: "")
+		if strippedText == string { return true }
+
+		textField.text = text.replacingCharacters(in: range, with: strippedText)
+
+		DispatchQueue.main.async {
+			let endPosition = textField.endOfDocument
+			let selectedTextRange = textField.textRange(from: endPosition, to: endPosition)
+			textField.selectedTextRange = selectedTextRange
+		}
+
+		return false
 	}
 
 }
